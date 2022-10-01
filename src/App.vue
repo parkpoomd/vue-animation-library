0c9170330a42006b24f7f71471f8b374049de709
parkpoomd <parkpoom.d@gmail.com>
2022-10-01 12:16:10 +0700

    completed

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<script setup>
+import { vAutoAnimate } from '@formkit/auto-animate';
+import { useAutoAnimate } from '@formkit/auto-animate/vue';
+import { ref } from 'vue';
+
+const isOpen = ref(false);
+const [wrapperEl] = useAutoAnimate();
+</script>
+
+<template>
+  <div
+    v-auto-animate="{ duration: 1000, easing: 'linear' }"
+    class="p-8 rounded-lg bg-neutral-900"
+  >
+    <h2 class="text-xl font-bold" @click="isOpen = !isOpen">
+      Why you should subscribe
+    </h2>
+    <p v-if="isOpen" class="py-8">I'm close to 20k so please 🙏</p>
+  </div>
+  <!-- <div ref="wrapperEl" class="p-8 rounded-lg bg-neutral-900">
+    <h2 class="text-xl font-bold" @click="isOpen = !isOpen">
+      Why you should subscribe
+    </h2>
+    <p v-if="isOpen" class="py-8">I'm close to 20k so please 🙏</p>
+  </div> -->
+</template>
